94a562c605c012a1f6819222fc9ec9d6e9e6fc6f
Rid <shakeel.ridhwaan@gmail.com>
2018-05-23 15:06:27 -0400

    add rspec

--- interviews/fibonacci.rb
+++
@@ -1,6 +0,0 @@
-def fib(n)
-   n <= 2 ? 1 : fib(n - 1) + fib(n - 2)
-end
-(1..20).each { |n| puts fib(n) }
-
-# TODO add test cases
--- interviews/print-file.py
+++ interviews/print-file.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 import os
 
+'''Print file
+'''
 def main(prompt):
   filename = raw_input(prompt) #ask for filename using the given prompt
-  if(os.path.exists(filename)):
-    for line in open(filename, "r").readlines(): 
-    	print line
+  if validate(filename):
+    for line in open(filename.strip(), "r").readlines(): 
+        print line
   else:
     print "The file you selected does not exist, please try again"
-    main(prompt) #Repeat this function if the use did not give valid input
+    main(prompt) #repeat this function if the use did not give valid input
 
-raw_input("Press enter to transfer the files...")
+def validate(input):
+    input = input.strip()
+    if input is None:
+        return False
+    elif input == '':
+        return False
+    elif not (os.path.exists(input)):
+        return False
+    else:
+        return True
 
 if __name__ == "__main__":
     main("Select file in current directory:")
-
-# TODO add test cases
