248815f26020f4eeefa49780bcb57805579f4b19
Rid <shakeel.ridhwaan@gmail.com>
2018-05-26 16:35:02 -0400

    reverse string java

---
+++ exercism/java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,22 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+    	inputString = validate(inputString);
+        int counter = inputString.length()-1;
+        String reverseString = "";
+        while (counter >= 0) {
+        	reverseString = reverseString + inputString.charAt(counter);
+        	counter--;
+        }
+        return reverseString;
+    }
+
+    private String validate(String input) {
+    	if (input == null){
+    		throw new NullPointerException("Input is null");
+    	}
+    	input = input.trim();
+    	return input;
+    }
+}
+
