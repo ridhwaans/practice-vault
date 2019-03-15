528ee53b882f8d771ae4fa2f225672dc6dd32083
Rid <shakeel.ridhwaan@gmail.com>
2019-03-14 21:05:20 -0400

    create stack class to get O(1) complexity

--- interviews/apple-2018/smallestInteger.js
+++ interviews/apple-2018/smallestInteger.js
@@ -2,24 +2,27 @@
 Given a dynamic arraylist which contains integers, create a solution which finds the minimum value integer in O(1) complexity (not O(n))
 */
 smallestInteger = (stack,numbers,k) => {
-	if (numbers[k] > stack[stack.length-1]){
-		stack.pop();
+	if (stack.length == 0){
 		stack.push(numbers[k]);
 	}
-	console.log("k:" + k);
+	if (numbers[k] <= stack[stack.length-1]){
+		stack.pop();
+		stack.push(numbers[k]);
+	} 
 	return (k == 0) ? stack[stack.length-1] : smallestInteger(stack,numbers,k-1);
 }
 
-var smallestInteger = function(numbers) {
-	let stack = [];
-	console.log(numbers); // is zero
+var smallestIntegerMainFunction = function(numbers) {
+	let stack = []; 
 	let smallest = smallestInteger(stack,numbers,numbers.length - 1);
 	return `the smallest integer is ${smallest}`;
 }
 
-var testCases = [[10,2,5,7,15,9], [1,2,3,4]];
+var testCases = [[10,2,5,7,15,9,-34,66,-22], [99,1,2,3,4]];
 for (let testCase of testCases){
-	console.log(testCase);
-	console.log(smallestInteger(testCase));
+	console.log(smallestIntegerMainFunction(testCase));
 }
 
+/*
+Time complexity is O(n)
+*/
