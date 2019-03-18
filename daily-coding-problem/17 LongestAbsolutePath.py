6ec0fc7898d5241f70ddaea16bcf8ad02c1bddb6
Rid <shakeel.ridhwaan@gmail.com>
2019-03-18 18:16:09 -0400

    fix 7 and 17

--- daily-coding-problem/17 LongestAbsolutePath.py
+++ daily-coding-problem/17 LongestAbsolutePath.py
@@ -30,3 +30,41 @@ The name of a file contains at least a period and an extension.
 
 The name of a directory or sub-directory will not contain a period.
 '''
+
+'''
+Assumptions:
+1) The longest absolute path can be a single file in the root level without directories
+2) The extension of the file does not necessarily have to be .ext
+'''
+import re
+
+def longestAbsolutePath(string):
+    longest = None
+    paths = re.compile(r'\.[a-zA-Z0-9]{2,4}').split(string)
+    for path in range(0,len(paths)):
+        print paths[path]
+        special = re.search(r'(\\n(\\t)*)',paths[path])
+        print special
+        valid = True
+        if len(special.groups()) > 1:
+            index = 0
+            while valid is True:
+                if (index > 0) and (special.groups(index) == special.groups(index-1) + '\\t'):
+                    valid = True
+                else:
+                    valid = False
+                index += 1
+        else:
+            if special.group(0) != '\\n':
+                valid = False
+        if valid == False:
+            paths.pop(path)
+        else:
+            paths[path] = re.sub(r'\\n(\\t)*','/', paths[path])
+            if (longest is None or longest < len(paths[path])):
+                longest = paths[path]
+    return longest
+
+
+input_string = r'dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext'
+print "Given {0}\n The longest absolute path is {1}".format(input_string, longestAbsolutePath(input_string))
--- daily-coding-problem/7 AlphabetCode.js
+++ daily-coding-problem/7 AlphabetCode.js
@@ -6,39 +6,57 @@ For example, the message '111' would give 3, since it could be decoded as 'aaa',
 You can assume that the messages are decodable. For example, '001' is not allowed.
  */
  
-AlphabetCode = function() {
-	var mapping = {}
-	var sentence = 'abcdefghijklmnopqrstuvwxyz';
-	for (var i = 0; i < sentence.length; i++) {
-	  mapping[sentence.charAt(i)] = (sentence.charCodeAt(i) -96)
-	}
 
+
+AlphabetCode = function() {
+	this.decodings = [];
+	this.mapping = {};
+	var letters = 'abcdefghijklmnopqrstuvwxyz';
+	for (var i = 0; i < letters.length; i++) {
+	  this.mapping[letters.charAt(i)] = (letters.charCodeAt(i) - 96)
+	}
 }
+
+AlphabetCode.prototype.getDecodings = function() {
+  return this.decodings;
+};
 
 AlphabetCode.prototype.decode_helper = function(message,k) {
 	
-	if (k == 0 || k == 1) return 1;
+	if (k == 0) return 1; // "" is 1
 
-	index = message.length - k; // current index left to right sequentially
-	console.log("k", k, "current character",parseInt(message.charAt(index)));
+	index = message.length - k;
+	if (parseInt(message.charAt(index)) == 0){
+		return 0; // starting with "0" is 0
+	}
+	
+	this.decodings.push(
+		parseInt(message.charAt(index))
+		);
 
 	result = AlphabetCode.prototype.decode_helper(message, k-1);
 	if ((k >= 2) && (parseInt(message.substring(index,index+2)) <= 26)){
 		result += AlphabetCode.prototype.decode_helper(message, k-2);
+
+		this.decodings.push(
+			parseInt(message.substring(index,index+2))
+			);
+
 	}
 	return result; 
 };
 
 AlphabetCode.prototype.decode = function(message) {
-	// BASE CASE: if empty or contains zeroes, it is not valid
-	if (/^[1-9]+$/.test(message) == false){
+	if (/^[0-9]+$/.test(message) == false){
 		return "invalid"
 	}
+
 	return AlphabetCode.prototype.decode_helper(message,message.length);
 }
 
 var alphabetCode = new AlphabetCode();
-console.log("result", alphabetCode.decode('112'));
+console.log(alphabetCode.getDecodings
+console.log("result", alphabetCode.decode('12345'));
 
 
 //aab kb al lb
