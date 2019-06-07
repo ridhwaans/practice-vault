bf72c6379d3a65f3c77b75c33256871a8821203a
Rid <shakeel.ridhwaan@gmail.com>
2019-06-07 15:53:10 -0400

    use collections counter to permute

---
+++ interviews/c3-2019/interview.py
@@ -0,0 +1,28 @@
+# Find all permutations of a list of letters
+from collections import Counter
+
+letters = ['A','B','C']
+def permuteUnique(letters):
+    def btrack(path, counter):
+        if len(path)==len(letters):
+            ans.append(path[:])
+        for x in counter:  # dont pick duplicates
+            if counter[x] > 0:
+                path.append(x)
+                counter[x] -= 1
+                btrack(path, counter)
+                path.pop()
+                counter[x] += 1
+    ans = []
+    btrack([], Counter(letters))
+    return ans
+
+print(permuteUnique(letters))
+'''
+Time complexity: O(n*n!) There are n! permutations and O(n) to print a permutation
+See also:
+https://docs.python.org/3/library/collections.html#collections.Counter
+'''
+
+# Implement a linkedlist data structure for a supply chain system https://leetcode.com/problems/design-linked-list/
+# Design a data model for a library reservation system to checkout, renew and return books
