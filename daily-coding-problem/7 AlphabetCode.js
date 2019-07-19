1b935f3be2ba51b4420f6bcf5221b14a90299088
Rid <shakeel.ridhwaan@gmail.com>
2019-07-19 15:43:25 -0400

    dp decode ways

--- daily-coding-problem/7 AlphabetCode.js
+++ daily-coding-problem/7 AlphabetCode.js
@@ -6,57 +6,37 @@ For example, the message '111' would give 3, since it could be decoded as 'aaa',
 You can assume that the messages are decodable. For example, '001' is not allowed.
  */
  
-
-
-AlphabetCode = function() {
-	this.decodings = [];
-	this.mapping = {};
-	var letters = 'abcdefghijklmnopqrstuvwxyz';
-	for (var i = 0; i < letters.length; i++) {
-	  this.mapping[letters.charAt(i)] = (letters.charCodeAt(i) - 96)
-	}
-}
-
-AlphabetCode.prototype.getDecodings = function() {
-  return this.decodings;
-};
-
-AlphabetCode.prototype.decode_helper = function(message,k) {
-	
-	if (k == 0) return 1; // "" is 1
-
-	index = message.length - k;
-	if (parseInt(message.charAt(index)) == 0){
-		return 0; // starting with "0" is 0
-	}
-	
-	this.decodings.push(
-		parseInt(message.charAt(index))
-		);
-
-	result = AlphabetCode.prototype.decode_helper(message, k-1);
-	if ((k >= 2) && (parseInt(message.substring(index,index+2)) <= 26)){
-		result += AlphabetCode.prototype.decode_helper(message, k-2);
-
-		this.decodings.push(
-			parseInt(message.substring(index,index+2))
-			);
-
-	}
-	return result; 
-};
-
+var AlphabetCode = function(){};
 AlphabetCode.prototype.decode = function(message) {
-	if (/^[0-9]+$/.test(message) == false){
-		return "invalid"
+	//dp is optimal substructure, account for zero length string such as dp[0]
+	let dp = Array(1+message.length).fill(0);
+	//string is of length zero
+	dp[0] = 0; // no ways to decode empty 
+	//string is of length one
+	dp[1] = parseInt(message[0]) == 0 ? 0 : 1; // there is no alphabet code for 0, 'a' starts at 1 
+
+	//string is of length two or greater
+	// go up to string length inclusive because index 0 is reserved for zero string
+	for (let i = 2; i <= message.length; i++){
+		let singleDigit = message.substring(i-1, i);
+		let doubleDigit = message.substring(i-2, i);
+		console.log(singleDigit + ' ' + doubleDigit);
+		//console.log(singleDigit[0]);
+		if (singleDigit[0] !='0' && 1 <= parseInt(singleDigit) <= 9){
+			dp[i] += dp[i-1];
+			console.log(dp[i]);
+		}
+		//console.log(doubleDigit[0]);
+		if (doubleDigit[0] !='0' && 10 <= parseInt(doubleDigit) <= 26){
+			//console.log('double valid');
+			dp[i] += dp[i-2];
+		}
 	}
-
-	return AlphabetCode.prototype.decode_helper(message,message.length);
-}
-
-var alphabetCode = new AlphabetCode();
-console.log(alphabetCode.getDecodings
-console.log("result", alphabetCode.decode('12345'));
-
-
-//aab kb al lb
+	// filled out the dp array and returning the accumulation of all subproblems
+	return dp[message.length];
+};
+combinations = new AlphabetCode();
+console.log('Number of ways to decode 10 are: (1) ' + combinations.decode('10'));
+console.log('Number of ways to decode 12 are: (2) ' + combinations.decode('12'));
+console.log('Number of ways to decode 226 are: (3) ' + combinations.decode('226'));
+console.log('Number of ways to decode 27 are: (1) ' + combinations.decode('27'));
