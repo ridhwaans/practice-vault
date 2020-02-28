56de79fdb64865abc01b09d0c93fb197bc90afcb
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2020-02-28 11:34:06 -0800

    largest three

--- interviews/apple-2018/largest_three.py
+++ interviews/apple-2018/largest_three.py
@@ -1,6 +1,7 @@
 '''
 Given list of integers, assume sorted or unsorted, find the largest three elements
 '''
+import random
 def largest_three(numbers):
     first, second, third = None, None, None
     temp = None
@@ -10,11 +11,20 @@ def largest_three(numbers):
             third = second
             second = first
             first = i
-        if temp > third:
-            third = temp
-        if temp > i:
-            
-    print 'three largest in {0} are: {1}'.format(numbers, (first, second, third))
+        if i > second and i < first:
+            temp = third
+            third = second
+            second = i
+        if i > third and i < second:
+            temp = third
+            third = i
+    return [first, second, third]
 
-largest_three([8,3,4,54,32,1,2,99,32]) 
+test_cases = ([8,3,4,54,32,1,2,99,32], [1,2,3], [234,65,34,-1,34,23,98])
+numbers = random.sample(range(0, 100), 10)
+print '\n'.join('the largest three in {0} are: {1}'.format(x, largest_three(x)) for x in test_cases)
 
+'''
+Time complexity is O(n)
+Space complexity is O(1)
+'''
