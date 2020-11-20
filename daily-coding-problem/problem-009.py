ac0ac0a35f5bdce07b69a149f967ce97d533a279
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2020-11-19 21:30:15 -0500

    dp solution

--- daily-coding-problem/problem-009.py
+++ daily-coding-problem/problem-009.py
@@ -11,17 +11,28 @@ Follow-up: Can you do this in O(N) time and constant space?
 """
 
 def dcp9(numbers):
-    incl, excl = 0, 0
-    for num in numbers:
-       temp = max(incl, excl)
-       incl = num + excl
-       excl = temp
-    return (max(incl, excl))
+    if len(numbers) == 1:
+        return numbers[0]
+    if len(numbers) == 2:
+        return max(numbers[0], numbers[1])
+
+    largest = [None] * len(numbers)
+    largest[0] = numbers[0]
+    largest[1] = max(numbers[0], numbers[1])
+    for i, num in enumerate(numbers):
+        if i > 1: 
+            largest[i] = max(largest[i-2] + numbers[i], largest[i-1])
+    return largest[len(numbers) - 1]
 
 numbers = [2, 4, 6, 2, 5]
+print(f"Given {numbers}, largest sum of non-adjacent numbers is {dcp9(numbers)}")
+numbers = [5, 1, 1, 5]
 print(f"Given {numbers}, largest sum of non-adjacent numbers is {dcp9(numbers)}")
 
 '''
 Time complexity is O(N)
 Space complexity is O(1)
+
+See also:
+https://www.youtube.com/watch?v=xlvhyfcoQa4
 '''
