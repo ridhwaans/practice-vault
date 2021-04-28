642a2166246d6311e45941330a7bbcee4c220859
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2021-04-27 21:45:44 -0400

    java

--- README.md
+++ README.md
@@ -7,15 +7,14 @@ Sourced from https://www.dailycodingproblem.com/
 ### exercism
 ```
 brew update && brew install exercism
-exercism configure --key=YOUR_API_KEY
-exercism configure --dir=.../exercise-vault/exercism
-exercism fetch <language>
-exercism submit <path_to_file>
-http://exercism.io/tracks/<language>/exercises/<exercise_name>
+exercism configure --token=<YOUR_CLI_TOKEN>
+exercism configure --dir=<path/to/practice-vault>/exercism
+exercism download --exercise=<exercise_name> --track=<language>
+exercism submit </path/to/file>
+https://exercism.io/tracks/<language>/exercises/<exercise_name>
 ```
 Sourced from https://exercism.io/
 
-### leetcode
-
 ### interviews
 
+### leetcode
---
+++ interviews/apple-2021/anadrome.java
@@ -0,0 +1,81 @@
+/* 
+Given a string word, count the minimum number of characters that needs to be added to word to make it anadrome - a string which is an anagram of some palindrome. 
+
+Examples:
+word = "tatoo"
+0
+"tatoo" is already an anagram of a palindrome "otato"
+
+word = "abcb"
+1
+Add either 'a' or 'c'. "abcba" is palindrome already, "abcbc" is an anagram of a palindrome "cbabc"
+
+word = "abab"
+0
+"abab" is already an anagram of a palindrome "abba"
+
+word = "abc"
+2
+Add 'a' and 'b' to the string word. "abcab" is an anagram of a palindrome "abcba"
+*/
+
+/*
+tatoo
+t -> 2
+a -> 1
+o -> 2
+otato
+
+abcb
+a -> 2
+b -> 2
+c -> 1
+abcba
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+  public static int minimumCount(String input) {
+    if(input == null || input.trim().isEmpty()) {
+      return 0;
+    }
+    Map<Character, Integer> even = new HashMap<>();
+    Map<Character, Integer> odd = new HashMap<>();
+    for(char c:input.toCharArray()) {
+      if(even.containsKey(c)) {
+        odd.put(c, even.remove(c)+1);
+      } else if(odd.containsKey(c)) {
+        even.put(c, odd.remove(c)+1);
+      } else {
+        odd.put(c,1);
+      }
+    }
+    System.out.println("Characters needed for " + input);
+    if(odd.size()>1) {
+      int count = odd.keySet().size();
+      for(char c: odd.keySet()) {
+        System.out.println(c);
+        count--;
+        if(count == 1) break;
+      }
+    }
+    return (odd.size()>1)? odd.size()-1:0;
+  }
+  public static void main(String[] args) {
+    System.out.println(minimumCount(null) == 0);
+    System.out.println(minimumCount(" ") == 0);
+    System.out.println(minimumCount("") == 0);
+    System.out.println(minimumCount("tatoo") == 0);
+    System.out.println(minimumCount("abcb") == 1);
+    System.out.println(minimumCount("abab") == 0);
+    System.out.println(minimumCount("abc") == 2);
+  }
+}
+
+/*
+Complexity: 
+time: O(n)
+space: O(n)
+*/
