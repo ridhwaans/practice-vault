606070a8f7e96c108d43f1744207267c89f7572f
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2021-05-18 10:49:51 -0400

    prob 204

--- interviews/other/node-sharing.py
+++ interviews/other/node-sharing.py
@@ -14,6 +14,77 @@ Implement a release_hardware function that does the following:
 - given a valid node whose state is "in use", releases the node back to the server cluster, and
 - sets the state of the released node as "free"
 - if the given node is already set to a state of "free", it cannot be released
+
 Ending question:
 What is the time/space complexity of the data structure/algorithm?
+'''
+
+from collections import defaultdict
+class Node:
+    def __init__(self, node_type):
+        self.node_type = node_type
+        self.state = 1 # [0,1] if 0 in_use, 1 is free
+        
+class Server:
+    def __init__(self, nodes_dict):
+        self.available_nodes = defaultdict(list)
+        self.in_use_nodes = defaultdict(list)
+        self.prepare_cluster(nodes_dict)
+        
+    def prepare_cluster(self, num_of_nodes): # O(N*3) time
+        for node_type, node_count in num_of_nodes.items():
+            
+            self.available_nodes[node_type] = [Node(node_type) for _ in range(node_count)]
+            
+    
+    def request_hardware(self, node_type): # O(1) time
+        if len(self.available_nodes[node_type]) == 0:
+            print('This node type is not available')
+            return 
+        
+        node = self.available_nodes[node_type].pop()
+        node.state = 0
+        self.in_use_nodes[node_type].append(node)
+        
+        return node
+    
+    def release_hardware(self, node): # O(N) time
+        if node.state == 1:
+            print('The node is already available')
+            return 
+        
+        node_type = node.node_type
+        
+        num_in_use_nodes = len(self.in_use_nodes[node_type])
+        index = 0
+        for index in range(num_in_use_nodes):
+            if self.in_use_nodes[node_type][index] == node:
+                break
+        self.in_use_nodes[node_type][index], self.in_use_nodes[node_type][-1] = self.in_use_nodes[node_type][-1], self.in_use_nodes[node_type][index]
+        
+        self.in_use_nodes[node_type].pop()
+        
+        node.state = 1
+        self.available_nodes[node_type].append(node)
+        print('Node released successfully')
+    
+    
+num_nodes = {'T1': 2, 'T2':0, 'T3': 0}
+server = Server(num_nodes)
+
+node1 = server.request_hardware('T1')
+print('node1 state: ',node1.state)
+node2 = server.request_hardware('T1') 
+print('node2 state: ',node2.state)
+
+node3 = server.request_hardware('T1')
+server.release_hardware(node2)
+print('node2 state: ',node2.state)
+
+'''
+space complexity: 
+O(N) N is the total number of nodes
+time complexity: 
+request hardware O(1)                
+release hardware O(N)
 '''
