068b83db0fc25d9773468556879cb52567091b8d
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2026-05-28 16:26:25 -0700

    fix bugs in binary search and max subarray

--- leetcode/33_search_in_rotated_sorted_array.cpp
+++ leetcode/33_search_in_rotated_sorted_array.cpp
@@ -2,29 +2,32 @@
 
 class Solution {
 public:
-    int search(std::vector<int>& nums, int target) {
-            int l = 0;
-            int r = nums.size() - 1;
-            while (l <= r ) {
-                int mid = l + ((r - l)/2); // prevents int overflow in naive (l + r)/2 
-                if (nums[mid] == target){
-                        return mid;
-                }
-                if (nums[l] <= nums[mid]){ //<= instead of < to avoid l or r == mid and time limit exceeded infinite loop
-                        if (nums[l] <= target && target < nums[mid]){
-                                r = mid - 1;
-                        } else {
-                                l = mid + 1;
-                        }
-                 }
-                if (nums[mid] < nums[r]){
-                        if (nums[mid] <= target && target < nums[r]){
-                                l = mid + 1;
-                        } else {
-                                r = mid - 1;
-                        }
-                    }
-            }
-            return -1;
-}
+  int search(vector<int> &nums, int target) {
+    // binary search with a lot of discrete cases
+    int l = 0;
+    int r = nums.size() - 1;
+    while (l <= r) {
+      int mid = l + ((r - l) / 2); // prevents int overflow in naive (l + r)/2
+      if (nums[mid] == target) {
+        return mid;
+      }
+      // left sorted portion
+      if (nums[l] <= nums[mid]) { //<= instead of < to avoid l or r == mid and
+                                  //time limit exceeded infinite loop
+        if (nums[l] <= target && target < nums[mid]) {
+          r = mid - 1;
+        } else {
+          l = mid + 1;
+        }
+        // right sorted portion
+      } else {
+        if (nums[mid] < target && target <= nums[r]) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      }
+    }
+    return -1;
+  }
 };
--- leetcode/53_maximum_subarray.cpp
+++ leetcode/53_maximum_subarray.cpp
@@ -1,14 +1,17 @@
 #include <algorithm>
+#include <climits>
+#include <vector>
+
 using namespace std;
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int cur_max = 0, global_max = 0;
-        for (const int& i : nums) {
-            cur_max = max(cur_max + i, i);
-            global_max = max(global_max, cur_max);
-        }
-        return global_max;
+  int maxSubArray(vector<int> &nums) {
+    int cur_max = 0, global_max = INT_MIN;
+    for (const int &i : nums) {
+      cur_max = max(cur_max + i, i);
+      global_max = max(global_max, cur_max);
     }
+    return global_max;
+  }
 };
