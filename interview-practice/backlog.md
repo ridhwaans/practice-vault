ead5e8564dc96bcc750cd9bb7f80cce695685481
Rid Shakeel <shakeel.ridhwaan@gmail.com>
2026-09-14 10:16:28 -0700

    practice questions

---
+++ interview-practice/backlog.md
@@ -0,0 +1,10 @@
+#### backlog
+
+```
+https://x.com/deedydas/status/1873944793732309489
+https://x.com/avrldotdev/status/2097611018591740132
+https://x.com/BenLesh/status/2099491001148625106
+https://x.com/coolcoder56/status/2083840770382147654
+https://x.com/coolcoder56/status/2082707454383751305
+```
+
---
+++ interview-practice/geometric_median_grid.py
@@ -0,0 +1,22 @@
+'''
+####################
+#         #       #
+#   *     #   *   #
+#         #       #
+#   #     #       #
+#   #             #
+#   #         *   #
+#   #     #       #
+#         #       #
+#   *     #       #
+####################
+
+In this 20x11 grid:
+
+• '#' represents walls
+• '*' represents target points
+• Empty spaces are where you can place your solution point
+
+The goal would be to find the coordinates of the point that minimizes the sum of distances to all four target points (denoted by '*'), while respecting that you can't pass through walls ('#'). This makes it a more
+Source: https://x.com/deedydas/status/1873944793732309489?s=20
+'''
